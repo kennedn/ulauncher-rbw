da811598d662a1fdfcbe3fa677798b479801da00
Varac <varac@varac.net>
2024-11-10 08:50:16 +0100

    Catch rbw list exceptions

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 import logging
 import subprocess
+import time
 
 from ulauncher.api import Extension, Result
 from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
@@ -34,10 +35,23 @@ class RbwExtension(Extension):
 
 if __name__ == "__main__":
     logging.info("rbw extension: started")
-    entries_str = subprocess.check_output(
-        ["rbw", "list", "--fields", "id,name,user"]  # noqa: S607
-    ).decode("utf-8")
-    entries_raw = entries_str.splitlines()
-    entries = [entry.split("\t") for entry in entries_raw]
-    logging.info("rbw extension: Loaded entries %s", len(entries))
+    # TODO: Remove while loop when https://github.com/Ulauncher/Ulauncher/issues/1063
+    # is merged
+    entries = []
+    while entries == []:
+        try:
+            entries_str = subprocess.check_output(
+                ["rbw", "list", "--fields", "id,name,user"]  # noqa: S607
+            ).decode("utf-8")
+            entries_raw = entries_str.splitlines()
+            entries = [entry.split("\t") for entry in entries_raw]
+            logging.info("rbw extension: Loaded entries %s", len(entries))
+        except subprocess.CalledProcessError as err:
+            logging.critical("rbw extension: rbw list failed: Pin entry cancelled ?")
+            logging.critical(err.output)
+        except subprocess.Exception as err:
+            logging.critical("rbw extension: rbw list failed: Unkown execption:")
+            logging.critical(err.output)
+        time.sleep(1)
+
     RbwExtension().run()
